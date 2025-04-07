4604c3e7a860be85e2e1c88c03f80e3f9b0c41b4
Hasinur <hasinur.rahman1332@gmail.com>
2025-04-07 22:41:22 +0600

    customized event creation api

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -2,11 +2,18 @@ import Event from "../models/Event.js";
 
 export const createEvent = async (req, res, next) => {
   const event = req.body;
+  event.status = "upcoming";
+  event.volunteersJoined = 0;
 
   try {
     const newEvent = new Event(event);
     await newEvent.save();
-    res.status(201).json(newEvent);
+    res.status(201).json({
+      success: true,
+      error: false,
+      message: "Event created successfully",
+      data: { event: newEvent },
+    });
   } catch (error) {
     next(error);
   }
